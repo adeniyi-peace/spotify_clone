1ce89e48cda428b8fd043468e8d76ed419b998f2
Adeniyi Peace <adeniyipeaceo1@gmail.com>
2025-01-21 17:27:44 +0100

    added javascript to album html to control background color of album-image div

--- test_html/static/album.js
+++ test_html/static/album.js
@@ -1,40 +1,41 @@
-const image = document.getElementById("album-image")
-const img_bg = document.querySelector(".album-image > img")
 
-image.onload = () =>{
-    // create off screen canvas
-    const canvas = document.createElemen("canvas")
-    const context = canvas.getContext("2d")
-    canvas.width = image.width
-    canvas.height = image.height
-    context.drawImage(image, 0, 0)
+const albumImage = document.getElementById('album-image');
+const image = document.querySelector('.album-img > img');
 
-    // get image data
-    const image_data = context.getImageData(0,0,canvas.width,canvas.height)
-    const pixels = image_data.data
-
-    // ccalculate color frequency
-    const colour_count = {}
-    for (let i = 0; i < pixels.length; i++) {
-        const r = pixels[i]
-        const g = pixels[i + 1]
-        const b = pixels[i + 2]
-        const color_key = "rgb($(r),$(g),$(b))"
-        colour_count[color_key] = (colour_count[color_key] || 0) + 1  
+window.onload = () => {
+    // Create an off-screen canvas
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    canvas.width = image.width;
+    canvas.height = image.height;
+    context.drawImage(image, 0, 0);
+  
+    // Get image data
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    const pixels = imageData.data;
+  
+    // Calculate color frequencies
+    const colorCounts = {};
+    for (let i = 0; i < pixels.length; i += 4) {
+      const r = pixels[i];
+      const g = pixels[i + 1];
+      const b = pixels[i + 2];
+      const colorKey = `rgb(${r},${g},${b})`;
+      colorCounts[colorKey] = (colorCounts[colorKey] || 0) + 1;
     }
-
-
-    // find most frequent color
-    let most_frequent_color = null
-    let max_count = 0
-    for (const colour in colour_count) {
-        if(colour_count[colour] > max_count) {
-            most_frequent_color = colour
-            max_count = colour_count[colour]
-        }
+  
+    // Find most frequent color
+    let mostFrequentColor = null;
+    let maxCount = 0;
+    for (const color in colorCounts) {
+      if (colorCounts[color] > maxCount) {
+        mostFrequentColor = color;
+        maxCount = colorCounts[color];
+      }
     }
-
-    if (most_frequent_color) {
-        img_bg.style.backgroundColor = most_frequent_color
+  
+    // Set album-image background color
+    if (mostFrequentColor) {
+      albumImage.style.backgroundColor = mostFrequentColor;
     }
-}
+  };
